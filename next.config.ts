4513741191d2f34670355f8211336d79d0181f20
Otto G <5030059+ottodevs@users.noreply.github.com>
2025-05-16 03:35:34 +0200

    ✨ feat(config): add critters for CSS optimization and refactor webpack externals handling

--- next.config.ts
+++ next.config.ts
@@ -5,15 +5,15 @@ const externals = ['pino-pretty']
 const nextConfig: NextConfig = {
     // Silence warnings
     // https://github.com/WalletConnect/walletconnect-monorepo/issues/1908
-    webpack: (config: NextConfig['webpack'] & { externals: string[] }) => ({
-        ...config,
-        externals: [...(config?.externals ?? []), ...externals],
-    }),
+    webpack: (config: NextConfig['webpack'] & { externals: string[] }) => {
+        config.externals.push(...externals)
+        return config
+    },
     turbopack: { rules: { externals } },
     images: { remotePatterns: [{ hostname: 'randomuser.me' }] },
     serverExternalPackages: externals,
 
-    // Optimize CSS loading
+    // Optimize CSS loading (requires critters to be installed)
     experimental: {
         optimizeCss: true,
     },
